9f631cc9e3cebc34aecfb1ce220b59247087e399
alejandrovelasco6831 <alejandrovelasco6831@gmail.com>
2026-07-03 19:13:41 -0500

    Crear endpoint listar clientes

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+
+clientes = []
+
+@app.get("/")
+def inicio():
+    return {"mensaje": "API funcionando"}
+
+@app.get("/clientes")
+def listar_clientes():
+    return clientes
