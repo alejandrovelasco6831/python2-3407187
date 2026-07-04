c5667993802d0cace45990779bdbbbed3f76a8ec
alejandrovelasco6831 <alejandrovelasco6831@gmail.com>
2026-07-03 20:35:19 -0500

    CRUD facturas con SQLModel

--- modelos/facturas.py
+++ modelos/facturas.py
@@ -1,7 +1,16 @@
-from pydantic import BaseModel
+from typing import Optional
+from sqlmodel import SQLModel, Field
 
-class Factura(BaseModel):
-    id: int
+
+class FacturaBase(SQLModel):
     fecha: str
     valor_total: float
-    cliente: str
+    cliente_id: int
+
+
+class Factura(FacturaBase, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+
+
+class FacturaCreate(FacturaBase):
+    pass
--- modelos/transacciones.py
+++ modelos/transacciones.py
@@ -1,7 +1,16 @@
-from pydantic import BaseModel
+from typing import Optional
+from sqlmodel import SQLModel, Field
 
-class Transaccion(BaseModel):
-    id: int
+
+class TransaccionBase(SQLModel):
     vr_unitario: float
     cantidad: int
     factura_id: int
+
+
+class Transaccion(TransaccionBase, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+
+
+class TransaccionCreate(TransaccionBase):
+    pass
